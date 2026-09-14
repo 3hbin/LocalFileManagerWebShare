4f2d2288a615990ad9956aefaa7d62d48bf363a0
3hbin <vn812013@gmail.com>
2026-09-14 21:50:33 +0700

    Upload LocalFileManagerWebShare project

--- app/src/main/java/com/localfm/app/EditorDialogs.kt
+++ app/src/main/java/com/localfm/app/EditorDialogs.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.viewinterop.AndroidView
 import java.io.File
 
